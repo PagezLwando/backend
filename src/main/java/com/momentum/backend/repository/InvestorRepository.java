9e15f0205a5f7a01c4d97b43bf54de34840c7fa8
PagezLwando <lwandonodume@gmail.com>
2023-10-08 15:21:32 +0200

    Added a getall in InvestorRepository

---
+++ src/main/java/com/momentum/backend/repository/InvestorRepository.java
@@ -0,0 +1,14 @@
+package com.momentum.backend.repository;
+
+import com.momentum.backend.models.Investor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InvestorRepository extends CrudRepository<Investor, Integer> {
+    @Query(value = "exec spGetAllInvestors", nativeQuery = true)
+    List<Investor> getAllInvestors();
+}
