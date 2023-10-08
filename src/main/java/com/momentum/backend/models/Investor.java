8b1ca62af98c7c8425f5f7a68d47d3eb551c65c0
PagezLwando <lwandonodume@gmail.com>
2023-10-08 17:55:17 +0200

    Added variables on Investor.java

--- src/main/java/com/momentum/backend/models/Investor.java
+++ src/main/java/com/momentum/backend/models/Investor.java
@@ -1,4 +1,25 @@
 package com.momentum.backend.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
 public class Investor {
+    @Id
+    private Integer id;
+    private String name;
+    private String surname;
+    private String dob;
+    private String cellNumber;
+    private String email;
+    private String address_id;
 }
