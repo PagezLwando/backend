073575e56b1d3c015623266bbe238c1904f1a7cb
PagezLwando <lwandonodume@gmail.com>
2023-10-09 13:33:39 +0200

    Added getAll stored procedures

---
+++ src/main/resources/db/migration/R__spGetAllInvestors.sql
@@ -0,0 +1,3 @@
+SELECT id, name, surname, date_of_birth, cell_number, email, address_id
+	FROM public.investor
+ORDER BY name ASC
---
+++ src/main/resources/db/migration/R__spGetAllProducts.sql
@@ -0,0 +1,3 @@
+SELECT id, type, current_balance, investor_id
+	FROM public.product
+ORDER BY id ASC
