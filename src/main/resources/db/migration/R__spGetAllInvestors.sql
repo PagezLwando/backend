4ac84981c322b018f051c90bc0f36e5dac9b5c9e
PagezLwando <lwandonodume@gmail.com>
2023-10-18 20:04:49 +0200

    Added GetAll endpooint

--- src/main/resources/db/migration/R__spGetAllInvestors.sql
+++ src/main/resources/db/migration/R__spGetAllInvestors.sql
@@ -1,3 +1,9 @@
-SELECT id, name, surname, date_of_birth, cell_number, email, address_id
-	FROM public.investor
-ORDER BY name ASC
+CREATE OR REPLACE PROCEDURE getAllInvestors()
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        PERFORM id, name, surname, date_of_birth, cell_number, email, address_id
+            FROM public.investor
+        ORDER BY name ASC;
+    END;
+$$;
--- src/main/resources/db/migration/R__spGetAllProducts.sql
+++ src/main/resources/db/migration/R__spGetAllProducts.sql
@@ -1,3 +1,13 @@
-SELECT id, type, current_balance, investor_id
-	FROM public.product
-ORDER BY id ASC
+-- PROCEDURE: public.getallproducts()
+
+-- DROP PROCEDURE IF EXISTS public.getallproducts();
+
+CREATE OR REPLACE PROCEDURE public.getAllProducts()
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+    PERFORM id, type, current_balance, investor_id
+    FROM product
+    ORDER BY id ASC;
+END;
+$$;
